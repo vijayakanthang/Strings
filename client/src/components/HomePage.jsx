6dacb28c3c154c00002a2d696c7652c6b86e13f8
vijayakanthang <vijayakanthang@gmail.com>
2025-08-05 16:36:28 +0530

    url fix

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -4,92 +4,101 @@ import axios from 'axios'
 import logo from '../assets/th.png'
 import { useNavigate } from 'react-router-dom'
 import Header from './Header'
-import {jwtDecode} from "jwt-decode";
-import {toast,ToastContainer} from "react-toastify"
-import  like  from "../assets/heartm.png";
+import { jwtDecode } from "jwt-decode"
+import { toast, ToastContainer } from "react-toastify"
+import like from "../assets/heartm.png"
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"
 
 const HomePage = () => {
-    const [username,setUsername] = useState()
-    const [new_thread,setNew_thread] = useState()
+    const [username, setUsername] = useState()
+    const [new_thread, setNew_thread] = useState()
     const [thread, setThread] = useState([])
-    const navigate= useNavigate();
-
-    
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const token = localStorage.getItem("token");
-                if(!token){
-                    navigate("/login"); 
+                if (!token) {
+                    navigate("/login");
                     return;
                 }
-                const response = await axios.get("http://localhost:8080/home/",{headers:{Authorization: `${token}`}})
-                setThread(response.data)
-                
-                const decodedtoekn = jwtDecode(token)
-                const username = decodedtoekn.username;
-                setUsername(username);
 
-            }
-            catch (error) {
+                const response = await axios.get(`${BASE_URL}/home/`, {
+                    headers: { Authorization: `${token}` }
+                })
+                setThread(response.data)
+
+                const decodedToken = jwtDecode(token)
+                setUsername(decodedToken.username)
+
+            } catch (error) {
                 console.error(error)
-                navigate("/login"); 
-                
+                navigate("/login")
             }
         }
+
         fetchData()
     }, [])
 
     // POST--- SubmitButton
-
-    const handleSubmit =async()=>{
-        if (new_thread){
-            const response = await axios.post("http://localhost:8080/",{username:username,new_thread:new_thread})
-            toast.success("posted")
-            setThread([...thread,response.data])
-            setNew_thread('');
+    const handleSubmit = async () => {
+        if (new_thread) {
+            const response = await axios.post(`${BASE_URL}/`, {
+                username: username,
+                new_thread: new_thread
+            })
+            toast.success("Posted")
+            setThread([...thread, response.data])
+            setNew_thread('')
         }
     }
 
     // Like
     const handleLike = async (threadId) => {
         try {
-            const response = await axios.put(`http://localhost:8080/api/threads/${threadId}`, { like: [username] });
-            console.log(response.data);
-            
+            const response = await axios.put(`${BASE_URL}/api/threads/${threadId}`, {
+                like: [username]
+            })
+            console.log(response.data)
         } catch (error) {
-            console.error('Error liking the thread:', error);
+            console.error('Error liking the thread:', error)
         }
-    };
-
-
+    }
 
     return (
         <>
-        <Header />
-        <div className='tot'>
-                     
-                <form onSubmit={() => handleSubmit(new_thread)}  className='input' >
-                <img src={logo} className='imgip'></img>
-                <input type='text' value={new_thread} placeholder='Start a thread...' className='inputfield' onChange={(e)=> setNew_thread(e.target.value)}></input>
-                <button className='postbtn' type='submit' >POST</button>
+            <Header />
+            <div className='tot'>
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    handleSubmit()
+                }} className='input'>
+                    <img src={logo} className='imgip' />
+                    <input
+                        type='text'
+                        value={new_thread}
+                        placeholder='Start a thread...'
+                        className='inputfield'
+                        onChange={(e) => setNew_thread(e.target.value)}
+                    />
+                    <button className='postbtn' type='submit'>POST</button>
                 </form>
-            
 
-            <div className='feed'>
-                {thread.map((t,index) => (
-                    <div className='card' key={t._id}>
-                        <h3>{t.username}</h3>
-                        <span>{t.date}</span>
-                        <p>{t.new_thread}</p>
-                        <button onClick={(e)=>handleLike(t._id)}><img src={like} className='like'></img></button>
-                    </div>
-                ))}
-
+                <div className='feed'>
+                    {thread.map((t, index) => (
+                        <div className='card' key={t._id}>
+                            <h3>{t.username}</h3>
+                            <span>{t.date}</span>
+                            <p>{t.new_thread}</p>
+                            <button onClick={() => handleLike(t._id)}>
+                                <img src={like} className='like' />
+                            </button>
+                        </div>
+                    ))}
+                </div>
             </div>
-            </div>
-        
         </>
     )
 }
--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
-// app.js
-
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // not needed if you're already using express.json()
 const { connectToDatabase } = require('./db');
 const { accessToken } = require('./middleware/auth');
 const { signup, login } = require('./controllers/userController');
-const { getThreads, addThread, updateThread, deleteThread, likeThread } = require('./controllers/threadsController');
+const {
+    getThreads,
+    addThread,
+    updateThread,
+    deleteThread,
+    likeThread
+} = require('./controllers/threadsController');
 
 const app = express();
 app.use(cors());
@@ -14,15 +18,19 @@ app.use(express.json());
 
 connectToDatabase();
 
+// Auth routes
 app.post('/signup', signup);
 app.post('/login', login);
+
+// Thread routes
 app.get('/', getThreads);
 app.get('/home', accessToken, getThreads);
-app.put('/:id',likeThread)
 app.post('/', addThread);
-app.put('/:id', updateThread);
+app.put('/api/update/:id', updateThread);     // <-- updated
+app.put('/api/threads/:id', likeThread);      // <-- updated
 app.delete('/:id', deleteThread);
 
+// Start server
 app.listen(8080, () => {
-    console.log('Server running');
+    console.log('Server running ');
 });
