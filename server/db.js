56bbdd800f65c01b77f396553eb4bdb2a64f99a5
vijayakanthang <vijayakanthang@gmail.com>
2025-08-05 16:28:38 +0530

    db url fix

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 async function connectToDatabase() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Thread', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect('mongodb://127.0.0.1:27017/Strings', { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('MongoDB connection error:', error);
